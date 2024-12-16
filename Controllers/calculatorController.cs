fd358f04f746b1e29d427bd15df56881d642e537
Hp <Hp@DESKTOP-0EGL18S>
2024-12-16 13:02:19 +0500

    Add project files.

---
+++ Controllers/calculatorController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace calculator.Controllers
+{
+    public class calculatorController : Controller
+    {
+        // GET: calculator
+        public ActionResult addition()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult addition(FormCollection obj)
+        {
+            try
+            {
+                int num1 = Convert.ToInt32(obj["txtnum1"]);
+                int num2 = Convert.ToInt32(obj["txtnum2"]);
+                string operation = obj["operation"];
+
+                string result = string.Empty;
+
+                switch (operation)
+                {
+                    case "Add":
+                        result = $"The sum is {num1 + num2}";
+                        break;
+
+                    case "Subtract":
+                        result = $"The difference is {num1 - num2}";
+                        break;
+
+                    case "Multiply":
+                        result = $"The product is {num1 * num2}";
+                        break;
+
+                    case "Divide":
+                        if (num2 != 0)
+                            result = $"The quotient is {num1 / num2}";
+                        else
+                            result = "Division by zero is not allowed.";
+                        break;
+
+                    default:
+                        result = "Invalid operation selected.";
+                        break;
+                }
+
+                ViewBag.data = result;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.data = "Error: " + ex.Message;
+            }
+
+            return View();
+        }
+    }
+}
+   
